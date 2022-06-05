9910ac7017a54252c187168049e23e8b0b18e66b
DrSkyFox <52832487+DrSkyFox@users.noreply.github.com>
2022-06-05 21:38:10 +0300

    spring commit add

---
+++ src/main/java/fts/acmc/AccessControlModuleCoreApplication.java
@@ -0,0 +1,13 @@
+package fts.acmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessControlModuleCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessControlModuleCoreApplication.class, args);
+	}
+
+}
